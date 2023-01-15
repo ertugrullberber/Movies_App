38f92bd76bd4639483b9b0d3cdb0685c3158f8df
ertugrullberber <ertugrulberber3461@gmail.com>
2023-01-15 15:01:30 +0300

    Creating Main View Model

--- Movie_App/AppDelegate.swift
+++ Movie_App/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let window = UIWindow(frame: UIScreen.main.bounds)
         let navigationController = UINavigationController(rootViewController: MainViewController())
-        window.rootViewController = MainViewController()
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         
         self.window = window
---
+++ Movie_App/ViewModel/MainVIewModel.swift
@@ -0,0 +1,20 @@
+//
+//  MainVIewModel.swift
+//  Movie_App
+//
+//  Created by Ertugrul Berber on 15.01.2023.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSection() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        5
+    }
+    
+}
