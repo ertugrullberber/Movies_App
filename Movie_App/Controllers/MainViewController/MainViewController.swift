15d9cd6c09d8ae8091361aea2742ec82d2c13070
ertugrullberber <ertugrulberber3461@gmail.com>
2023-01-15 14:34:32 +0300

    Deleting SceneDelegate and main.storyboard

---
+++ Movie_App/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  Movie_App
+//
+//  Created by Ertugrul Berber on 15.01.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.title = "Main View"
+        self.view.backgroundColor = .red
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
