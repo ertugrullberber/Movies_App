70c001411b6f30058355400ff0e2fa38b61aeb6f
ertugrullberber <ertugrulberber3461@gmail.com>
2023-01-15 14:14:22 +0300

    first commit

---
+++ Movie_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie_App
+//
+//  Created by Ertugrul Berber on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
